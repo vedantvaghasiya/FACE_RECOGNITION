8f861739449cffd4d413312870d5c4ac9810070d
vedantvaghasiya <vedantvaghasiya@gmail.com>
2025-02-01 13:24:19 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import cv2
+
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+imgBackground = cv2.imread('Resources/background.png')
+
+# Importing the mode images into a list
+folderModePath = 'Resources/Modes'
+modePathList = os.listdir(folderModePath)
+imgModeList = []
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
+# print(len(imgModeList))
+
+
+
+
+while True:
+    success, img = cap.read()
+
+    imgBackground[162:162 + 480, 55:55 + 640] = img
+
+    # cv2.imshow("Webcam", img)
+    cv2.imshow("Face Attendance", imgBackground)
+    cv2.waitKey(1)
